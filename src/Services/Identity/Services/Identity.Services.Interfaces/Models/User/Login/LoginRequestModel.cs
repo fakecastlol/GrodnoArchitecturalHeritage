4a499ea51a2896296237ef2d58d80f441b0dc558
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-19 12:21:10 +0300

    commented unused code

--- src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/Login/LoginRequestModel.cs
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/Login/LoginRequestModel.cs
@@ -4,11 +4,11 @@ namespace Identity.Services.Interfaces.Models.User.Login
 {
     public class LoginRequestModel
     {
-        [Required(ErrorMessage = "Email not specified")]
+        //[Required(ErrorMessage = "Email not specified")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password not specified")]
-        [DataType(DataType.Password)]
+        //[Required(ErrorMessage = "Password not specified")]
+        //[DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
--- src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/Register/RegisterRequestModel.cs
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/Register/RegisterRequestModel.cs
@@ -4,16 +4,16 @@ namespace Identity.Services.Interfaces.Models.User.Register
 {
     public class RegisterRequestModel
     {
-        [Required(ErrorMessage = "Email not specified")]
+        //[Required(ErrorMessage = "Email not specified")]
         //[EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password not specified")]
-        [DataType(DataType.Password)]
+        //[Required(ErrorMessage = "Password not specified")]
+        //[DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Incorrect password")]
+        //[DataType(DataType.Password)]
+        //[Compare("Password", ErrorMessage = "Incorrect password")]
         public string ConfirmPassword { get; set; }
     }
 }
