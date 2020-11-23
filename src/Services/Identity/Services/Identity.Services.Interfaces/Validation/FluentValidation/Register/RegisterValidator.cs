93b618e6b0f599b2eef7419934a0643c87e42837
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-23 19:35:45 +0300

    changed RegisterValidation

--- src/Services/Identity/Services/Identity.Services.Interfaces/Validation/FluentValidation/Register/RegisterValidator.cs
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Validation/FluentValidation/Register/RegisterValidator.cs
@@ -12,15 +12,22 @@ namespace Identity.Services.Interfaces.Validation.FluentValidation.Register
                 .EmailAddress().WithMessage("Invalid email format.");
 
             RuleFor(user => user.Password)
-                .NotEmpty().WithMessage("Password is required.")
-                .Matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
-                .WithMessage("Minimum eight characters, at least one letter and one number:");
+                .NotEmpty().WithMessage("Password is required.");
+                //.Matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
+                //.WithMessage("Minimum eight characters, at least one letter and one number:");
 
-            RuleFor(user => user.ConfirmPassword)
-                .NotEmpty().WithMessage("Confirm Password is required.");
+            //RuleFor(user => user.ConfirmPassword)
+            //    .NotEmpty().WithMessage("Confirm Password is required.");
 
-            RuleFor(user => user.ConfirmPassword)
-                .Equal(user => user.Password).WithMessage("Passwords should match.");
+            //RuleFor(user => user.ConfirmPassword)
+            //    .Equal(user => user.Password).WithMessage("Passwords should match.");
+            //RuleFor(x => x).Custom((x, context) =>
+            //{
+            //    if (x.Password != x.ConfirmPassword)
+            //    {
+            //        context.AddFailure(nameof(x.Password), "Passwords should match");
+            //    }
+            //});
         }
     }
 }
