360051258cd538d81ed8c81c964e486fa3443b94
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-25 18:15:22 +0300

    added user fields

--- src/Services/Identity/Domain/Identity.Domain.Core/Entities/UserEntity.cs
+++ src/Services/Identity/Domain/Identity.Domain.Core/Entities/UserEntity.cs
@@ -1,4 +1,5 @@
 ﻿using Identity.Domain.Core.Entities.Abstract;
+using System;
 
 namespace Identity.Domain.Core.Entities
 {
@@ -7,5 +8,15 @@ namespace Identity.Domain.Core.Entities
         public string Email { get; set; }
         public string Password { get; set; }
         public Roles Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime LastVisited { get; set; }
+        public string Location { get; set; }
+        public int ArticleCount { get; set; }
+        public int MessagesCount { get; set; }
+        public int Rank { get; set; }
+        public string Avatar { get; set; }
     }
 }
