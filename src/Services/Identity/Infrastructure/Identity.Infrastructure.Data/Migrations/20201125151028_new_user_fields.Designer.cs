15a25c2e1fca87aca68f89696e68734935d35f79
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-25 18:15:41 +0300

    added migration

--- src/Services/Identity/Infrastructure/Identity.Infrastructure.Data/Migrations/20201125151028_new_user_fields.Designer.cs
+++ src/Services/Identity/Infrastructure/Identity.Infrastructure.Data/Migrations/20201125151028_new_user_fields.Designer.cs
@@ -4,14 +4,16 @@ using Identity.Infrastructure.Data.EFContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Identity.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20201125151028_new_user_fields")]
+    partial class new_user_fields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
