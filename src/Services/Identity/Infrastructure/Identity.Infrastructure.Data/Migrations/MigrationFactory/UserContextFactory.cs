ab1cfc46970f7eaf322aac98b4a59ad5499a8d09
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-25 18:15:06 +0300

    added migration factory

---
+++ src/Services/Identity/Infrastructure/Identity.Infrastructure.Data/Migrations/MigrationFactory/UserContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Identity.Infrastructure.Data.EFContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Identity.Infrastructure.Data.Migrations.MigrationFactory
+{
+    public class UserContextFactory : IDesignTimeDbContextFactory<UserContext>
+    {
+        public UserContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UserContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=localhost, 14330;Database=UserDB;User=sa;Password=Password_111ssassSsvc111;TrustServerCertificate=true");
+
+            return new UserContext(optionsBuilder.Options);
+        }
+    }
+}
