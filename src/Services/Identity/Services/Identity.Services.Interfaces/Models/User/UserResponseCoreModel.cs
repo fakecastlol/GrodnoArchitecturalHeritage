c85b26bb890ee91c03b55399454186b43722f8ac
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-26 19:17:28 +0300

    added fields

--- src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/UserResponseCoreModel.cs
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/UserResponseCoreModel.cs
@@ -1,4 +1,5 @@
-﻿using Identity.Domain.Core.Entities;
+﻿using System;
+using Identity.Domain.Core.Entities;
 using Identity.Services.Interfaces.Models.User.Abstract;
 
 namespace Identity.Services.Interfaces.Models.User
@@ -8,5 +9,15 @@ namespace Identity.Services.Interfaces.Models.User
         public string Email { get; set; }
 
         public Roles Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime LastVisited { get; set; }
+        public string Location { get; set; }
+        public int ArticleCount { get; set; }
+        public int MessagesCount { get; set; }
+        public int Rank { get; set; }
+        public string Avatar { get; set; }
     }
 }
