6ea76189c6c5d6ea277db71ab4a29e03c8edddde
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-26 19:18:50 +0300

    added claims

--- src/Services/Identity/Services/Identity.Services.Interfaces/Models/Jwt/Claims.cs
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/Jwt/Claims.cs
@@ -4,6 +4,16 @@
     {
         Id = 1,
         Email = 2,
-        Scope = 3
+        Scope = 3,
+        FirstName = 4,
+        LastName = 5,
+        Login = 6,
+        RegistrationDate = 7,
+        LastVisited = 8,
+        Location = 9,
+        ArticleCount = 10,
+        MessagesCount = 11,
+        Rank = 12,
+        Avatar = 13
     }
 }
