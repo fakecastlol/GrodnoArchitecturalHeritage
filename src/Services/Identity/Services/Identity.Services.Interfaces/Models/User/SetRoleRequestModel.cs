e24070eebb55d40e36a672641752bd715937244d
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-30 19:55:14 +0300

    added setRoleRequestmodel

---
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/SetRoleRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Identity.Domain.Core.Entities;
+using Identity.Services.Interfaces.Models.User.Abstract;
+
+namespace Identity.Services.Interfaces.Models.User
+{
+    public class SetRoleRequestModel : CoreModel
+    {
+        public Roles Role { get; set; }
+    }
+}
