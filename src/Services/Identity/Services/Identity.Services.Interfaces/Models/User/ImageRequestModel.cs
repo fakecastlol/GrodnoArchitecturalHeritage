8f77f8972fe18ed95bf78ec44e4667ea2b2c944c
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-01 20:16:23 +0300

    added imageRequestModel

---
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/ImageRequestModel.cs
@@ -0,0 +1,9 @@
+﻿using Identity.Services.Interfaces.Models.User.Abstract;
+
+namespace Identity.Services.Interfaces.Models.User
+{
+    public class ImageRequestModel : CoreModel
+    {
+        public string Avatar { get; set; }
+    }
+}
