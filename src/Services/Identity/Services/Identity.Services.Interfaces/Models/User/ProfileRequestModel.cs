a35e393c8aac4f6cd74fd57a9038e6fb10598ac0
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-01 20:16:11 +0300

    added profileIRequestModel

---
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/ProfileRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Services.Interfaces.Models.User.Abstract;
+
+namespace Identity.Services.Interfaces.Models.User
+{
+    public class ProfileRequestModel : CoreModel
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Location { get; set; }
+    }
+}
