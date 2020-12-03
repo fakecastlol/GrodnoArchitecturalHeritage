d1a98ddc592bd13ce4b675786a95fa189532868f
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:29:05 +0300

    added ConstructionRepository

---
+++ src/Services/Heritage/Infrastructure/Heritage.Infrastructure.Data/Repositories/ConstructionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Heritage.Domain.Core.Entities;
+using Heritage.Domain.Interfaces.Repositories;
+using Heritage.Infrastructure.Data.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace Heritage.Infrastructure.Data.Repositories
+{
+    public class ConstructionRepository : GenericRepository<Construction>, IConstructionRepository
+    {
+        public ConstructionRepository(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
