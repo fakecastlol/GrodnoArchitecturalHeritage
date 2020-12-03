1dad7c3f831e06449a6e414b6b249cce5a19439d
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:29:19 +0300

    added HeritageContextFactory

---
+++ src/Services/Heritage/Infrastructure/Heritage.Infrastructure.Data/Migrations/MigrationFactory/HeritageContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Heritage.Infrastructure.Data.EFContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Heritage.Infrastructure.Data.Migrations.MigrationFactory
+{
+    public class HeritageContextFactory : IDesignTimeDbContextFactory<HeritageContext>
+    {
+        public HeritageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<HeritageContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=localhost, 14330;Database=HeritageDB;User=sa;Password=Password_111ssassSsvc111;TrustServerCertificate=true");
+
+            return new HeritageContext(optionsBuilder.Options);
+        }
+    }
+}
