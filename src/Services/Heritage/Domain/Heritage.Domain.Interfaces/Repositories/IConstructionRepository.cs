0954eff9e955ba3eb1e16445c67fe68a96c11fa9
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:26:17 +0300

    added IConstructionRepository

---
+++ src/Services/Heritage/Domain/Heritage.Domain.Interfaces/Repositories/IConstructionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Heritage.Domain.Core.Entities;
+using Heritage.Domain.Interfaces.Repositories.Generic;
+
+namespace Heritage.Domain.Interfaces.Repositories
+{
+    public interface IConstructionRepository : IGenericRepository<Construction>
+    {
+    }
+}
