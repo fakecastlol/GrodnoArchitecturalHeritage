2fade18e73e8254eace50732f22bb38aaec7ac60
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:26:27 +0300

    added IImageRepository

---
+++ src/Services/Heritage/Domain/Heritage.Domain.Interfaces/Repositories/IImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using Heritage.Domain.Core.Entities;
+using Heritage.Domain.Interfaces.Repositories.Generic;
+
+namespace Heritage.Domain.Interfaces.Repositories
+{
+    public interface IImageRepository : IGenericRepository<Image>
+    {
+        
+    }
+}
