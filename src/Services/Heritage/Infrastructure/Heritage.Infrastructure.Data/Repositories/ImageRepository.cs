39478eae5103b2ff89418cfa056b3cb5c64b2e92
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:28:35 +0300

    added ImageRepository

---
+++ src/Services/Heritage/Infrastructure/Heritage.Infrastructure.Data/Repositories/ImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using Heritage.Domain.Core.Entities;
+using Heritage.Domain.Interfaces.Repositories;
+using Heritage.Infrastructure.Data.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace Heritage.Infrastructure.Data.Repositories
+{
+    public class ImageRepository : GenericRepository<Image>, IImageRepository
+    {
+        public ImageRepository(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
