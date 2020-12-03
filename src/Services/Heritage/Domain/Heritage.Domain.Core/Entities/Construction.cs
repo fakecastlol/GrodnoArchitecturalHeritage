1d38c03cc9b6386864b75c024dc9abc4311d8ec7
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:25:10 +0300

    added construction entity

---
+++ src/Services/Heritage/Domain/Heritage.Domain.Core/Entities/Construction.cs
@@ -0,0 +1,29 @@
+﻿using Heritage.Domain.Core.Entities.Abstract;
+using Heritage.Domain.Core.Entities.Enums;
+using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+
+namespace Heritage.Domain.Core.Entities
+{
+    public class Construction : BaseEntity
+    {
+        public Construction()
+        {
+            Images = new HashSet<Image>();
+        }
+
+        public string Name { get; set; }
+        public Types Type { get; set; }
+        public DateTime BuildDate { get; set; }
+        public DateTime LossDate { get; set; }
+        public LossCauses LossCause { get; set; }
+        public Materials Material { get; set; }
+        public string Article { get; set; }
+        public ArchitecturalStyles ArchitecturalStyle { get; set; }
+        //public Point Location { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; } /*= new List<Image>();*/
+    }
+}
