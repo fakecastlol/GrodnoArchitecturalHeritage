4a68f20138a8202f5d61661169b7519503ad676e
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:30:30 +0300

    added Heritage context

---
+++ src/Services/Heritage/Infrastructure/Heritage.Infrastructure.Data/EFContext/HeritageContext.cs
@@ -0,0 +1,21 @@
+﻿using Heritage.Domain.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Heritage.Infrastructure.Data.EFContext
+{
+    public class HeritageContext : DbContext
+    {
+        public DbSet<Construction> Constructions { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        public HeritageContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
