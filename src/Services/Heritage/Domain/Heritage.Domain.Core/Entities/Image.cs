08531779310f7d4f3439ceb3b3f2cdbb9dbe9ca9
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:24:58 +0300

    added image entity

---
+++ src/Services/Heritage/Domain/Heritage.Domain.Core/Entities/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Heritage.Domain.Core.Entities.Abstract;
+
+
+namespace Heritage.Domain.Core.Entities
+{
+    public class Image : BaseEntity
+    {
+        public string Name { get; set; }
+        public Guid ConstructionId { get; set; } 
+
+        public virtual Construction Construction { get; set; }
+    }
+}
