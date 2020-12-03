ffb607b95ca40ab405a634f1a6f445ec6ca1d8a9
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:26:05 +0300

    added IGenericRepository

---
+++ src/Services/Heritage/Domain/Heritage.Domain.Interfaces/Repositories/Generic/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Heritage.Domain.Interfaces.Repositories.Generic
+{
+    public interface IGenericRepository<TEntity>
+        where TEntity : class
+    {
+        Task<IQueryable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes = null);
+
+        Task<TEntity> GetByIdAsync(Guid id);
+
+        Task<TEntity> CreateAsync(TEntity item);
+        Task<TEntity> UpdateAsync(TEntity item);
+        Task DeleteAsync(Guid id);
+    }
+}
