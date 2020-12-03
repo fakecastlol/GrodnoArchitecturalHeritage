0df0fefcca6b30298d7a2e6ebb37803ffebf8243
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-03 17:30:41 +0300

    added dbInitializer

---
+++ src/Services/Heritage/Infrastructure/Heritage.Infrastructure.Data/EFContext/DbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Heritage.Infrastructure.Data.EFContext
+{
+    public static class DbInitializer
+    {
+        public static void Seed(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var context = scope.ServiceProvider.GetService<HeritageContext>();
+
+            context.Database.Migrate();
+
+            context.SaveChanges();
+        }
+    }
+}
