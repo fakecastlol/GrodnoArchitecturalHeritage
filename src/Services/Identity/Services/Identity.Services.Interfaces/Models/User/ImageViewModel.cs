3cb7e13cc4c12977e60bcb98d4cc338c4f45c0db
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-04 20:09:16 +0300

    changed model name

---
+++ src/Services/Identity/Services/Identity.Services.Interfaces/Models/User/ImageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Identity.Services.Interfaces.Models.User.Abstract;
+using Microsoft.AspNetCore.Http;
+
+namespace Identity.Services.Interfaces.Models.User
+{
+    public class ImageViewModel : CoreModel
+    {
+        public IFormFile File { get; set; }
+    }
+}
