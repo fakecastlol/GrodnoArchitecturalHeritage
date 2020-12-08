64221b4b3961f5558abe7357c341501a73f163b8
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-08 11:32:23 +0300

    added automapperprofile

---
+++ src/Services/Heritage/Infrastructure/Heritage.Infrastructure.Business/Support/Adapter/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Heritage.Domain.Core.Entities;
+using Heritage.Services.Interfaces.Models.Construction;
+
+namespace Heritage.Infrastructure.Business.Support.Adapter
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Construction, ConstructionResponseCoreModel>();
+            CreateMap<ConstructionResponseCoreModel, Construction>();
+        }
+    }
+}
