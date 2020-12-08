48679782037f6c26593177b2b0b6d1204c2316e6
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-08 11:28:28 +0300

    added services configuration

---
+++ src/ApiGateways/Web.Platform.HttpAggregator/Extensions/ServicesConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Web.Platform.HttpAggregator.Extensions
+{
+    public static class ServicesConfiguration
+    {
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            const string authenticationProviderKey = "Bearer";
+
+            services.AddAuthentication().AddJwtBearer(authenticationProviderKey, options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                };
+            });
+        }
+    }
+}
