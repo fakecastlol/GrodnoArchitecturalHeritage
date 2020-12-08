09e4527b288f1e4296f0d7e960e6cd6009fb5bf9
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-08 11:32:13 +0300

    added services

---
+++ src/Services/Identity/Infrastructure/Identity.Infrastructure.Business/Support/File/FileService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity.Services.Interfaces.Contracts;
+using Identity.Services.Interfaces.Helpers.AppSettings;
+using Identity.Services.Interfaces.Models.User;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Identity.Infrastructure.Business.Support.File
+{
+    public class FileService : IFileService
+    {
+        private readonly FileSettings _fileSettings;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public FileService(IOptions<FileSettings> fileSettings, IWebHostEnvironment hostEnvironment)
+        {
+            _fileSettings = fileSettings.Value;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public async Task<string> SaveImageFileAsync(ImageViewModel imageRequestModel)
+        {
+            var imageName = new string(Path.GetFileNameWithoutExtension(imageRequestModel.Id.ToString()).Take(10).ToArray()).Replace(' ', '-');
+            imageName = imageName + DateTime.Now.ToString(_fileSettings.SaveFormat) + Path.GetExtension(imageRequestModel.File.Name);
+
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, _fileSettings.Path, imageName);
+
+            await using var fileStream = new FileStream(imagePath, FileMode.Create);
+
+            await imageRequestModel.File.CopyToAsync(fileStream);
+
+            return imagePath;
+        }
+
+        public void DeleteImageFile(string imageName)
+        {
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, _fileSettings.Path, imageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+        }
+    }
+}
---
+++ src/Services/Identity/Infrastructure/Identity.Infrastructure.Business/Support/Token/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Identity.Services.Interfaces.Contracts;
+using Identity.Services.Interfaces.Helpers.AppSettings;
+using Identity.Services.Interfaces.Models.Jwt;
+using Identity.Services.Interfaces.Models.User;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Identity.Infrastructure.Business.Support.Token
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public TokenService(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateJwtToken(UserResponseCoreModel user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(Claims.Id.ToString().ToLower(), user.Id.ToString()),
+                    new Claim(Claims.Email.ToString().ToLower(), user.Email),
+                    new Claim(Claims.Scope.ToString().ToLower(), user.Role.ToString()),
+                    new Claim(Claims.FirstName.ToString().ToLower(), user.FirstName ?? string.Empty),
+                    new Claim(Claims.LastName.ToString().ToLower(), user.LastName ?? string.Empty),
+                    new Claim(Claims.Login.ToString().ToLower(), user.Login ?? string.Empty),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Audience = _jwtSettings.Audience,
+                Issuer = _jwtSettings.Issuer
+
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
