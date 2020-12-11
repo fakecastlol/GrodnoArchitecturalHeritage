a424a4b76432d772215dae2906e38a96ce31231f
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-11 12:04:53 +0300

    replaced dcode

--- src/Services/Identity/Services/Identity.Services.Interfaces/Helpers/AppSettings/FileSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Identity.Services.Interfaces.Helpers.AppSettings
-{
-    public class FileSettings
-    {
-        public string Path { get; set; }
-        public string SaveFormat { get; set; }
-        public string DefaultImage { get; set; }
-    }
-}
